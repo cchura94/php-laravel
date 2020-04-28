f310063e58eb59c31c68ac0a49b54cde50be3be8
cchura94 <cchura.cpc@gmail.com>
2020-04-28 18:04:33 -0400

    clase 1 y 2

---
+++ clase1/comando echo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Html y PHP</title>
+</head>
+<body>
+
+    <?php
+        // Esta linea es un comentario en php
+        echo "comentario en php";
+        /*
+            comenario 
+            en bloque
+        */
+        echo "<br>";
+        echo "comentario en bloque";
+
+    ?>
+    
+</body>
+</html>
---
+++ clase2/variables.php
@@ -0,0 +1,23 @@
+<?php
+    $mivariable = "Este es mi variable";
+    $_prueba = "prueba";
+
+    echo $mivariable;
+    echo "<br>";
+    echo $_prueba;
+    echo "<br>";
+
+    echo "Ejemplo: " . $mivariable;
+
+    echo "<h1>Tipos de Datos</h1>";
+    print("<h2>Entero</h2>");
+    //echo "Una", "cadena", "de", "texto", $_prueba;
+
+    $entero = 5;
+    $entero2 = -5;
+
+    echo "Esta variable es entero " . $entero;
+    echo "<br>";
+    $decimal = 5.432;
+    echo "Mi decimal es $decimal";
+?>
